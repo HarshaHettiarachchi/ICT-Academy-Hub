ee27f99e2a7c409b42e51da76539cc34e0e5ba71
HarshaHettiarachchi <hpchettiarachchi96@gmail.com>
2026-08-17 21:52:50 +0900

    Fix GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -13,69 +13,32 @@ import Projects from "./pages/Projects";
 import Resources from "./pages/Resources";
 import AskQuestion from "./pages/AskQuestion";
 
-
-// Main application component with routing setup
 export default function App() {
-
   return (
-
-    <BrowserRouter>
-
-      {/* Main application wrapper with global theme styles */}
+    <BrowserRouter basename="/ICT-Academy-Hub">
       <div
         className="
-        min-h-screen
-        bg-white
-        text-gray-900
-        dark:bg-gray-950
-        dark:text-white
-        transition-colors
-        duration-300
+          min-h-screen
+          bg-white
+          text-gray-900
+          dark:bg-gray-950
+          dark:text-white
+          transition-colors
+          duration-300
         "
       >
-
-        {/* Common navigation bar */}
         <Navbar />
 
-
-        {/* Application route configuration */}
         <Routes>
-
-          <Route
-            path="/"
-            element={<Home />}
-          />
-
-          <Route
-            path="/lessons"
-            element={<Lessons />}
-          />
-
-          <Route
-            path="/projects"
-            element={<Projects />}
-          />
-
-          <Route
-            path="/resources"
-            element={<Resources />}
-          />
-
-          <Route
-            path="/ask-question"
-            element={<AskQuestion />}
-          />
-
+          <Route path="/" element={<Home />} />
+          <Route path="/lessons" element={<Lessons />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/ask-question" element={<AskQuestion />} />
         </Routes>
 
-
-        {/* Common footer section */}
         <Footer />
-
       </div>
-
     </BrowserRouter>
-
   );
-
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,26 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "./index.css";
-
 import App from "./App.tsx";
 import { ThemeProvider } from "./context/ThemeContext";
 
-
-// Create React application root and render the app
-createRoot(
-  document.getElementById("root")!
-).render(
-
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-
-    {/* Global theme provider for light and dark mode control */}
     <ThemeProvider>
-
-      {/* Main application component */}
       <App />
-
     </ThemeProvider>
-
   </StrictMode>
-
 );
