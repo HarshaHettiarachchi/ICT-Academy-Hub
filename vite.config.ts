6e7161049a1e4f89c3458e10a27ddde054bfbd97
HarshaHettiarachchi <hpchettiarachchi96@gmail.com>
2026-08-17 22:18:04 +0900

    Fix GitHub Pages path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ICT-Academy-Hub/',
   plugins: [react()],
+  base: '/ICT-Academy-Hub/',
 })
