e1e3febcb7099edc4be50e0b3a3288443859ccd2
HarshaHettiarachchi <hpchettiarachchi96@gmail.com>
2026-08-17 21:38:24 +0900

    Configure GitHub Pages

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect, ReactNode } from "react";
+import { createContext, useState, useEffect, type ReactNode } from "react";
 
 // Props type for ThemeProvider component
 interface ThemeProviderProps {
