bc1bfa3d8f2b3adaf7645a3c05d979a552f1b8c1
HarshaHettiarachchi <hpchettiarachchi96@gmail.com>
2026-08-17 23:06:17 +0900

    Fix fixed navbar overlap

--- src/App.tsx
+++ src/App.tsx
@@ -19,13 +19,15 @@ export default function App() {
       <div className="min-h-screen bg-white text-gray-900 dark:bg-gray-950 dark:text-white transition-colors duration-300">
         <Navbar />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/lessons" element={<Lessons />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/resources" element={<Resources />} />
-          <Route path="/ask-question" element={<AskQuestion />} />
-        </Routes>
+        <main className="pt-20">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/lessons" element={<Lessons />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/resources" element={<Resources />} />
+            <Route path="/ask-question" element={<AskQuestion />} />
+          </Routes>
+        </main>
 
         <Footer />
       </div>
