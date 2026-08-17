bfe75bb49d35dfd77b5176c998f557b046fa1e90
HarshaHettiarachchi <hpchettiarachchi96@gmail.com>
2026-08-17 22:16:03 +0900

    Fix GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/ICT-Academy-Hub/",
-  
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-});
+  base: '/ICT-Academy-Hub/',
+  plugins: [react()],
+})
